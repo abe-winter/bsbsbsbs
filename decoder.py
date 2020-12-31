e76a3b6852ec4e143b11d5cf1d1ee51c42a2f377
Abe Winter <abe-winter@users.noreply.github.com>
2020-12-31 15:26:45 -0500

    de-antialias logic instead of thresholding

--- decoder.py
+++ decoder.py
@@ -7,11 +7,18 @@ class AllWhiteError(Exception): "content not found in image"
 
 Region = collections.namedtuple('Region', 'a b')
 
+def cluster_number(bars):
+    "takes array of bar lengths (in x-units standardized from start pattern). returns which cluster this belongs to, used for decoding or error checking maybe"
+    assert len(bars) == 4
+    return (bars[0] - bars[1] + bars[2] - bars[3]) % 9
+
 class Clip:
     def __init__(self):
         self.firstrow = None
         self.lastrow = None
         self.lineslices = None
+        self.raw_words = None
+        self.est_x = None
 
     def extract_lines(self, rows):
         "get identical line regions from image"
@@ -28,33 +35,85 @@ class Clip:
         self.lineslices = slices
         logging.debug('found %d lines', len(self.lineslices))
 
-    def parse_rows(self, rows):
+    def parse_raw_words(self, rows):
         assert self.lineslices is not None
-        for slice_ in self.lineslices:
-            yield self.parse_row(rows[slice_][0])
+        raw = [
+            self.row_words(rows[slice_][0])
+            for slice_ in self.lineslices
+        ]
+        self.raw_words, raw_x = zip(*raw)
+        self.est_x = sum(raw_x) / len(raw_x)
+        logging.info('raw words %s', [len(row) for row in self.raw_words])
+        logging.info('est_x %.2f', self.est_x)
 
-    def parse_row(self, row):
-        # note: checking red only (of RGBA) on the assumption this is pretty grayscale
-        threshed = [redpix < 128 for redpix in row[::4]]
+    @staticmethod
+    def row_words(row):
+        # note: checking red only (of RGBA) on the assumption this is nice grayscale
+        levels = [redpix for redpix in row[::4]]
+        assert levels[0] == 255 and levels[-1] == 255 # logic slightly depends on white borders
+        runs = [
+            (i, b)
+            for i, (a, b) in enumerate(zip([None] + levels, levels + [None]))
+            if a != b
+        ]
+        run_lengths = [(ib - ia, leva) for (ia, leva), (ib, levb) in zip(runs[:-1], runs[1:])]
+        # warning: this isn't checking for like 0, 1, 2, 4, which is a long gray region that indicates troubling scan
+        assert all(length == 1 or level in (0, 255) for length, level in run_lengths) # crappy anti-aliasing if this fails; todo: config flag to allow this
+        # note: this is now doing super-resolution logic for the imperfect pixels. This is slightly more accurate than just thresholding at 128
+        chunks = []
+        assert run_lengths[0][1] == 255 # or else the loop crashes because it doesn't append an initial chunk
+        for length, level in run_lengths:
+            if level == 255:
+                chunks.append([])
+            else:
+                chunks[-1].append((length, level))
+        # note: at this point space lengths are discarded. That's fine; we don't need them
+        # note: if any space is gray rather than white, this breaks
+        # todo: factor out this logic
+        bar_lengths = []
+        for chunk in chunks:
+            if not chunk:
+                continue
+            if len(chunk) == 1:
+                assert chunk[0][1] == 0
+            elif len(chunk) == 2:
+                assert sum(item[1] == 0 for item in chunk) == 1
+            elif len(chunk) == 3:
+                assert chunk[0][1] != 0 and chunk[2][1] != 0 and chunk[1][1] == 0
+            else:
+                raise NotImplementedError('chunks must be 1/2/3')
+            bar_lengths.append(sum(length * (1 - level / 255) for length, level in chunk))
+        print('bar_lengths', bar_lengths)
+        raise NotImplementedError
         # True means black
-        assert threshed[0] is False and threshed[-1] is False # logic slightly depends on white borders
+        assert threshed[0] is False and threshed[-1] is False 
         edges = [i for i, (a, b) in enumerate(zip(threshed[:-1], threshed[1:])) if a != b]
         blacks = [Region(*tup) for tup in zip(edges[::2], edges[1::2])]
-        print('blacks', blacks)
+
         # https://en.wikipedia.org/wiki/PDF417#Format
         start = blacks[:4] # start pattern
         stop = blacks[-5:] # stop pattern
         center = blacks[4:-5] # left row, codewords, right row
         singles = start[1:] + stop[1:]
         approxlen = sum(tup.b - tup.a for tup in singles) / len(singles)
-        limit = approxlen * 16 # pdf417 word starts with black bar, ends with white space, len 17. I'm using 16 to make room for antialiasing? prove this is robust
-        logging.debug('approxlen %s', approxlen)
-        words = [[]]
-        for region in center:
-            if words[-1] and region.b - words[-1][0].a > limit:
-                words.append([])
-            words[-1].append(region)
-        logging.debug('parsed %d words', len(words))
+        limit = approxlen * 16 * 1.1 # pdf417 word starts with black bar, ends with white space, len 17, last unit is always space so 16. 1.1 slippage because idk.
+        # logging.debug('approxlen %s', approxlen)
+        assert len(center) % 4 == 0
+
+        words = [center[4 * i:4 * (i+1)] for i in range(len(center) // 4)]
+        assert all(word[-1].b - word[0].a <= limit for word in words)
+
+        return words, approxlen
+
+    def parse_words(self):
+        "raw_words (list of chunks of regions) to pdf417 encoding"
+        # decoding rule is https://www.expresscorp.com/uploads/specifications/44/USS-PDF-417.pdf pg 3-4
+        for row in self.raw_words:
+            for chunk in row:
+                print('chunk', chunk)
+                x_unit_bars = [round((reg.b - reg.a) / self.est_x) for reg in chunk]
+                cluster = cluster_number(x_unit_bars)
+                print('cluster', cluster)
         raise NotImplementedError
 
     def infer_height(self, rows):
@@ -84,7 +143,8 @@ def decode_417(fname):
     clip.infer_height(rows)
     print(clip.firstrow, clip.lastrow)
     clip.extract_lines(rows)
-    parsed = list(clip.parse_rows(rows))
+    clip.parse_raw_words(rows)
+    clip.parse_words()
 
 def main():
     p = argparse.ArgumentParser(description=__doc__)

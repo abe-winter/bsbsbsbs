6ce6c7d05a66b1743fddc38e1a5d4fc9c2bc7274
Abe Winter <abe-winter@users.noreply.github.com>
2020-12-31 16:01:33 -0500

    load bar-space patterns copied from ocr pdf uh oh

--- decoder.py
+++ decoder.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 "decode pdf417 from a perfect clipped PNG. won't work on photos"
 
-import png, argparse, logging, collections
+import png, argparse, logging, collections, csv, time
 
 class AllWhiteError(Exception): "content not found in image"
 
@@ -70,7 +70,7 @@ class Clip:
         # note: at this point space lengths are discarded. That's fine; we don't need them
         # note: if any space is gray rather than white, this breaks
         # todo: factor out this logic
-        bar_lengths = []
+        bars = []
         for chunk in chunks:
             if not chunk:
                 continue
@@ -82,39 +82,47 @@ class Clip:
                 assert chunk[0][1] != 0 and chunk[2][1] != 0 and chunk[1][1] == 0
             else:
                 raise NotImplementedError('chunks must be 1/2/3')
-            bar_lengths.append(sum(length * (1 - level / 255) for length, level in chunk))
-        print('bar_lengths', bar_lengths)
-        raise NotImplementedError
-        # True means black
-        assert threshed[0] is False and threshed[-1] is False 
-        edges = [i for i, (a, b) in enumerate(zip(threshed[:-1], threshed[1:])) if a != b]
-        blacks = [Region(*tup) for tup in zip(edges[::2], edges[1::2])]
+            bars.append(sum(length * (1 - level / 255) for length, level in chunk))
 
         # https://en.wikipedia.org/wiki/PDF417#Format
-        start = blacks[:4] # start pattern
-        stop = blacks[-5:] # stop pattern
-        center = blacks[4:-5] # left row, codewords, right row
+        start = bars[:4] # start pattern
+        stop = bars[-5:] # stop pattern
+        center = bars[4:-5] # left row, codewords, right row
         singles = start[1:] + stop[1:]
-        approxlen = sum(tup.b - tup.a for tup in singles) / len(singles)
-        limit = approxlen * 16 * 1.1 # pdf417 word starts with black bar, ends with white space, len 17, last unit is always space so 16. 1.1 slippage because idk.
+        approxlen = sum(singles) / len(singles)
         # logging.debug('approxlen %s', approxlen)
+        # limit = approxlen * 16 * 1.1 # pdf417 word starts with black bar, ends with white space, len 17, last unit is always space so 16. 1.1 slippage because idk.
         assert len(center) % 4 == 0
 
         words = [center[4 * i:4 * (i+1)] for i in range(len(center) // 4)]
-        assert all(word[-1].b - word[0].a <= limit for word in words)
+        # assert all(word[-1].b - word[0].a <= limit for word in words) # todo: space lengths would be useful here for length-checking
 
         return words, approxlen
 
     def parse_words(self):
         "raw_words (list of chunks of regions) to pdf417 encoding"
         # decoding rule is https://www.expresscorp.com/uploads/specifications/44/USS-PDF-417.pdf pg 3-4
-        for row in self.raw_words:
-            for chunk in row:
-                print('chunk', chunk)
-                x_unit_bars = [round((reg.b - reg.a) / self.est_x) for reg in chunk]
-                cluster = cluster_number(x_unit_bars)
-                print('cluster', cluster)
+        for irow, row in enumerate(self.raw_words):
+            bar_chunks = [[round(bar / self.est_x) for bar in chunk] for chunk in row]
+            clusters = [cluster_number(chunk) for chunk in bar_chunks]
+            assert all(cluster in (0, 3, 6) for cluster in clusters)
+            assert all(cluster == 3 * (irow % 3) for cluster in clusters)
         raise NotImplementedError
+
+    def load_bs(self):
+        "bs = bar-space pattern. I copy-pasted this from an OCRed PDF so YTMND"
+        t0 = time.time()
+        bs = {}
+        for cluster in (0, 3, 6):
+            reader = csv.reader(open(f"cluster-{cluster}-bs.csv"))
+            lookup = {}
+            for pattern, symbol in reader:
+                lookup[pattern] = int(symbol)
+            assert list(lookup.values()) == list(range(len(lookup))) # i.e. expect ordering
+            assert all(len(pattern) == 8 for pattern in lookup.keys())
+            bs[cluster] = lookup
+        logging.debug('loaded bs tables in %.2fs', time.time() - t0)
+        self.bs = bs
 
     def infer_height(self, rows):
         # todo perf: only scan top-left tenth maybe
@@ -144,6 +152,7 @@ def decode_417(fname):
     print(clip.firstrow, clip.lastrow)
     clip.extract_lines(rows)
     clip.parse_raw_words(rows)
+    clip.load_bs()
     clip.parse_words()
 
 def main():
